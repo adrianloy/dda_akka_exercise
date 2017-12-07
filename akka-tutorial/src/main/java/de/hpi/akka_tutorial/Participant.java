10d6957240965e45e231947b7bcf1cef93fa9a4d
Felix Wolff <felix.wolff2@student.hpi.de>
2017-12-07 15:46:43 +0000

    Update Participant

--- akka-tutorial/src/main/java/de/hpi/akka_tutorial/Participant.java
+++ akka-tutorial/src/main/java/de/hpi/akka_tutorial/Participant.java
@@ -1,16 +1,19 @@
 package de.hpi.akka_tutorial;
 
-public class Participant {
+import java.io.Serializable;
 
-	
+public class Participant implements Serializable{
+	private static final long serialVersionUID = -123272840353304769L;
+
 	private int id; 
 	private String name;
 	private String pwhash;
 	private String dna;
 	private String pw_clear;
-	private Participant dna_match;
-	
-	
+	private int dna_match_partner_id;
+	private String dna_match;
+
+
 	public Participant(int id, String name, String pwhash, String dna) {
 		super();
 		this.id = id;
@@ -70,14 +73,20 @@ public class Participant {
 	}
 
 
-	public Participant getDna_match() {
+	public String getDna_match() {
 		return dna_match;
 	}
 
 
-	public void setDna_match(Participant dna_match) {
+	public void setDna_match(String dna_match) {
 		this.dna_match = dna_match;
 	}
-	
-	
+
+	public void setDna_match_partner_id(int id) { this.dna_match_partner_id = id; }
+	public int  getDna_match_partner_id() { return this.dna_match_partner_id; }
+
+	@Override
+	public String toString() {
+		return String.format("%s[%,d..%,d]", this.getClass().getSimpleName(), this.id, this.name);
+	}
 }
